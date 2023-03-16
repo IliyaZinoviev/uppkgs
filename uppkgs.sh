fd7185943846029e42cb343047426d8439f93c0b
iliyazinoviev <mrmar333@yandex.ru>
2023-03-16 11:21:00 +0300

    Исправлены недочёты. Добавлен обязательный позиционный параметр для указания конфигурации скрипта.

--- uppkgs.sh
+++ uppkgs.sh
@@ -1,5 +1,11 @@
-not_available="" &&
-for cmd in curl jq docker git poetry; do
+if [ -z "$1" ]
+  then
+    echo "Передайте абсолютный путь к конфигу." 1>&2
+    exit 1
+fi
+
+not_available=() &&
+for cmd in curl jq docker git poetry pytest; do
   if ! command -v "$cmd" &>/dev/null; then
     not_available+=("$cmd")
   fi
@@ -8,7 +14,8 @@ if (( ${#not_available[@]} > 0 )); then
   echo "Установите следующие пакеты: ${not_available[@]}" 1>&2
   exit 1
 fi &&
-config=$(jq -r '.config' ./.env.json) &&
+
+config=$1 &&
 source=$(jq -r '.source' $config) &&
 gitlab_host=$(jq -r '.gitlab_host' $config) &&
 repos=$(jq -r '.repo_keys' $config) &&
@@ -19,28 +26,30 @@ reviewer_ids=$(jq -r '.reviewer_ids' $config) &&
 source_branch=feature/${ticket}-packages-updating &&
 token=$(jq -r '.token' $config) &&
 packages=$(jq -r '.packages' $config)
-cd ~"$source" &&
-docker --version > /dev/null 2>&1 &&
+
+cd "$source" &&
 for repo in $(echo $repos | jq -r 'join(" ")')
 do
+
   repo_pkgs=$(jq -r '.repos."'$repo'".packages' $config) &&
   target_branch=$(jq -r '.repos."'$repo'".target_branch' $config) &&
   branch_title=$(echo $repo_pkgs | jq -r 'join("-")' ) &&
   source_branch=feature/${ticket}-${branch_title}-update &&
   project_id=$(jq -r '.repos."'$repo'".project_id' $config) &&
+
   cd ./$repo &&
   git stash -m "AUTOSTASH" &&
   git fetch -a &&
   git checkout -B $source_branch origin/$target_branch &&
   for package in $(echo $repo_pkgs | jq -r 'join(" ")')
   do
-    name=$(echo $packages | jq -r '."'$package'"')
-    poetry add $name
+    new_package=$(echo $packages | jq -r '."'$package'"')
+    poetry add new_package
   done &&
   source "$(poetry env info --path)"/bin/activate &&
-  pytest -x || read &&
+  pytest -x || read "?Почините тесты и после можете продолжить скрипт, нажав Enter" &&
   commit=(git commit -a -m "$ticket $commit_message." -m "[ci skip]") &&
-  $commit || $commit || read || $commit || $commit &&
+  $commit || $commit || read "?Поправьте формат кода и после можете продолжить скрипт, нажав Enter" || $commit || $commit &&
   git push origin HEAD &&
   body='{
           "title": "'$ticket' '$commit_message'",
